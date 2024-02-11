23c1c9a6b81e1bf1ef16567c5f6b37487eaa4169
Drew Ivey <drew.ivey4543@stu.mtec.edu>
2024-02-11 10:29:06 -0700

    updating bg image to png as I can't find out why git pages is not displaying the background

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'login-bg': "url('/images/login-bg.jpg')",
+        'login-bg': "url('/images/login-bg.png')",
       }
     },
   },
